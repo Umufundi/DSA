8f377c3172da1bae8abc5fad6e76a2295227d2ae
Flo <96528163+Umufundi@users.noreply.github.com>
2023-12-19 13:20:18 -0500

    Time: 166 ms (7.63%), Space: 17.9 MB (16.10%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -13,5 +13,16 @@ class Solution:
         maj = max(track, key = track.get)
         
         return maj
-        
+    
+    
+'''
+from collections import Counter
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = Counter(nums)
+        majority_element = max(count, key=count.get)
+        return majority_element
+
+'''
         
