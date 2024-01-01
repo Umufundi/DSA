28d51e87108ae9277adc579bf3b6cb971daa07a5
Flo <96528163+Umufundi@users.noreply.github.com>
2024-01-01 17:45:09 -0500

    Time: 283 ms (30.73%), Space: 17.8 MB (24.31%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -1,35 +1,21 @@
-class TrieNode:
-    def __init__(self, val="#"):
-        self.isEnd = False
-        self.children = dict()
-        self.val = val
-
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
-        root = TrieNode()
+        trie = {}
         for word in dictionary:
-            cur = root
-            for c in word:
-                if c not in cur.children:
-                    cur.children[c] = TrieNode(c)
-                cur = cur.children[c]
-            cur.isEnd = True
-        res = len(s)
-        dp = [len(s) for _ in range(len(s) + 1)]
-        dp[0] = 0
-        for start in range(len(s)):
-            pre = root
-            for end in range(start, len(s)):
-                if s[end] not in pre.children:
-                    dp[end + 1] = min(dp[end + 1], dp[end] + 1)
-                    break
-                else:
-                    pre = pre.children[s[end]]
-                    if pre.isEnd:
-                        #print(f"pre = {pre.val} start = {s[start]} end = {s[end]}")
-                        dp[end + 1] = min(dp[end + 1], dp[start])
-                    else:
-                        dp[end + 1] = min(dp[end + 1], dp[end] + 1)
+            current = trie
+            for char in word:
+                current = current.setdefault(char, {})
+            current[""] = None
 
-        #print(dp)
-        return dp[-1]
+        n = len(s)
+        dp = [0] * (n + 1)
+        for i in range(n)[::-1]:
+            dp[i] = dp[i + 1] + 1
+            current = trie
+            for j in range(i, n):
+                if s[j] not in current:
+                    break
+                current = current[s[j]]
+                if "" in current:
+                    dp[i] = min(dp[i], dp[j + 1])
+        return dp[0]
