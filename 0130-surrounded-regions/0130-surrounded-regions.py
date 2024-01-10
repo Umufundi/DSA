fb77e9a60f5a8dd45c59fe532cc26152b850a584
Flo <96528163+Umufundi@users.noreply.github.com>
2024-01-10 10:01:49 -0500

    Time: 113 ms (97.55%), Space: 18.6 MB (46.23%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -26,5 +26,29 @@ class Solution:
                 elif board [i][j] == 'Y':
                     board [i][j] = 'O'
                     
-       
+        """
+        def solve(self, board: List[List[str]]) -> None:
+        
+        def dfs(sv):
+            a,b=sv[0],sv[1]
+            board[a][b]='Y'
+            for r,c in [(a-1,b),(a+1,b),(a,b-1),(a,b+1)]:
+                if 0<=r<m and 0<=c<n and board[r][c]=='O':
+                    board[r][c]="Y"
+                    dfs((r,c))
+        m=len(board)
+        n=len(board[0])
+        for i in range(m):
+            for j in range(n):
+                if i==0 or i==m-1 or j==0 or j==n-1:
+                    if board[i][j]=='O':
+                        dfs((i,j))
+        for i in range(m):
+            for j in range(n):
+                if board[i][j]=='O':
+                    board[i][j]='X'
+                elif board[i][j]=='Y':
+                    board[i][j]='O'
+        """
+                    
        
