b5e8a484a333ad61ffe50a5c07200dce07d0a127
Flo <96528163+Umufundi@users.noreply.github.com>
2024-01-19 11:50:41 -0500

    Time: 39 ms (49.48%), Space: 16.5 MB (53.44%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,6 +1,8 @@
+from itertools import zip_longest
+
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        
+        '''  
         result = []
         for i, j in zip(word1, word2):
             result.append(i)
@@ -10,5 +12,9 @@ class Solution:
         result.extend(word2[len(word1):])
         
         return "".join(result)
-                
+             '''   
         
+
+
+        result = [char1 + char2 for char1, char2 in zip_longest(word1, word2, fillvalue='')]
+        return ''.join(result)
